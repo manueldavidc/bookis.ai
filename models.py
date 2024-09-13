221f77679987a95b916941d2e2e34a50b39256f2
manueldavidc <manuel.castillo@correlation-one.com>
2024-09-13 15:03:05 -0500

    Create models.py

---
+++ models.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255))
+    books = db.relationship('Book', backref='author', lazy=True)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    cover_image = db.Column(db.String(1000))
+    pdf_path = db.Column(db.String(1000))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
